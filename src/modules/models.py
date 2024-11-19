dba537ec259a917d29f52e8eb53b1d89159eef1f
Girish G N <girishgn12@gmail.com>
2024-11-18 21:57:56 -0500

    Adding DB Feature to Slash

---
+++ src/modules/models.py
@@ -0,0 +1,15 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+# Placeholder template for Wishlist - Can be changed
+class WishlistItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    item_name = db.Column(db.String(200), nullable=False)
+    price = db.Column(db.Float, nullable=True)
